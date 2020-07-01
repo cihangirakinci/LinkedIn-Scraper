023e9b51301a951616d80398ca917464c6c9af3e
cihangirakinci <akincicihangir@gmail.com>
2020-07-01 12:50:23 -0400

    Fix rubocop issue

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,7 +10,7 @@ class JobsController < ApplicationController
     @first = Job.new
     options = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
     driver = Selenium::WebDriver.for :firefox, options: options
-    
+
     driver.get('https://www.linkedin.com/jobs/search?keywords=%22ruby%20On%20Rails%22&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0&f_TP=1')
 
     job_titles = driver.find_elements(class: 'result-card__title')
@@ -18,11 +18,11 @@ class JobsController < ApplicationController
 
     job_titles.length.times do |i|
       job = Job.new
-      unless job_titles[i].text.include?("Senior") || job_titles[i].text.include?("Sr") || job_titles[i].text.include?("Lead")
-        job.title = job_titles[i].text
-        job.link = job_links[i].attribute('href')
-        job.save!
-      end
+      next if job_titles[i].text.include?('Senior') || job_titles[i].text.include?('Sr') || job_titles[i].text.include?('Lead')
+
+      job.title = job_titles[i].text
+      job.link = job_links[i].attribute('href')
+      job.save!
     end
 
     driver.quit

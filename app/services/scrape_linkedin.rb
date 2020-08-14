21c44e9c3caec6a492c98446a1b8b8b05fda4038
cihangirakinci <akincicihangir@gmail.com>
2020-08-13 23:04:29 -0400

    Add nokogiri

--- app/services/scrape_linkedin.rb
+++ app/services/scrape_linkedin.rb
@@ -2,8 +2,8 @@
 
 class ScrapeLinkedin
   def perform
-    set_up_selenium!
-    @driver.get('https://www.linkedin.com/jobs/search?keywords=%22ruby%20On%20Rails%22&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0&f_TP=1')
+
+    set_up_watir_nokogiri
 
     find_elements!
 
@@ -11,12 +11,14 @@ class ScrapeLinkedin
       next if @job_titles[title].text.include?('Senior') || @job_titles[title].text.include?('Sr') || @job_titles[title].text.include?('Lead') || @job_titles[title].text.include?('Director')
       next unless @company_names[title]
 
-      browser = Capybara::Session.new(:selenium_chrome_headless)
-      browser.visit(@job_links[title].attribute('href'))
-      next unless browser.has_css?('.show-more-less-html__markup')
-
-      desc = browser.find('.show-more-less-html__markup')['innerHTML']
+      explore = Watir::Browser.new
+      explore.goto @job_links[title].attribute('href')
+      browser = Nokogiri::HTML.parse(explore.html)
+      explore.close
+      next unless browser.css('.show-more-less-html__markup')
+      desc = browser.css('.show-more-less-html__markup').inner_html
       create_job!(title, desc)
+
     end
   end
 
@@ -32,14 +34,16 @@ class ScrapeLinkedin
     job.save!
   end
 
-  def set_up_selenium!
-    options = Selenium::WebDriver::Chrome::Options.new(args: ['-headless'])
-    @driver = Selenium::WebDriver.for :chrome, options: options
+  def set_up_watir_nokogiri
+    explore = Watir::Browser.new
+    explore.goto 'https://www.linkedin.com/jobs/search?keywords=%22ruby%20On%20Rails%22&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0&f_TP=1'
+    @driver = Nokogiri::HTML.parse(explore.html)
+    explore.close
   end
 
   def find_elements!
-    @job_titles = @driver.find_elements(class: 'result-card__title')
-    @job_links = @driver.find_elements(class: 'result-card__full-card-link')
-    @company_names = @driver.find_elements(class: 'result-card__subtitle-link')
+    @job_titles = @driver.css('.result-card__title')
+    @job_links = @driver.css('.result-card__full-card-link')
+    @company_names = @driver.css('.result-card__subtitle-link')
   end
 end

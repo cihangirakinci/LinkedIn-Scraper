ff1f3f7c363a1c7523f387d68124a5b228a95588
cihangirakinci <akincicihangir@gmail.com>
2020-08-19 17:49:43 -0400

    Change Watir to Faraday

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'nokogiri'
-require 'watir'
 require 'date'
 
 class JobsController < ApplicationController
--- app/services/scrape_linkedin.rb
+++ app/services/scrape_linkedin.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'open-uri'
 
 class ScrapeLinkedin
   def perform
@@ -10,11 +11,11 @@ class ScrapeLinkedin
     @job_titles.length.times do |title|
       next if @job_titles[title].text.include?('Senior') || @job_titles[title].text.include?('Sr') || @job_titles[title].text.include?('Lead') || @job_titles[title].text.include?('Director')
       next unless @company_names[title]
-
-      explore = Watir::Browser.new
-      explore.goto @job_links[title].attribute('href')
-      browser = Nokogiri::HTML.parse(explore.html)
-      explore.close
+      next if Job.find_by(link: @job_links[title].attribute('href').value.split('?')[0])
+      
+      url = @job_links[title].attribute('href').value
+      explore = Faraday.get url
+      browser = Nokogiri::HTML.parse(explore.body)
       next unless browser.css('.show-more-less-html__markup')
       desc = browser.css('.show-more-less-html__markup').inner_html
       create_job!(title, desc)
@@ -27,7 +28,7 @@ class ScrapeLinkedin
   def create_job!(title, desc)
     job = Job.new
     job.title = @job_titles[title].text
-    job.link = @job_links[title].attribute('href')
+    job.link = @job_links[title].attribute('href').value.split('?')[0]
     job.company = @company_names[title].text
     job.description = desc
     job.date = Date.today
@@ -35,10 +36,10 @@ class ScrapeLinkedin
   end
 
   def set_up_watir_nokogiri
-    explore = Watir::Browser.new
-    explore.goto 'https://www.linkedin.com/jobs/search?keywords=%22ruby%20On%20Rails%22&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0&f_TP=1'
-    @driver = Nokogiri::HTML.parse(explore.html)
-    explore.close
+    url = 'https://www.linkedin.com/jobs/search?keywords=%22ruby%20On%20Rails%22&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0&f_TP=1'
+    explore = Faraday.get url
+
+    @driver = Nokogiri::HTML.parse(explore.body)
   end
 
   def find_elements!
